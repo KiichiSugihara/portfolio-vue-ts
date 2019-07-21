7a4052513debe0e220d86f03040f35512e2350b3
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-07-22 08:32:21 +0900

    Add(environment): add Button

---
+++ src/components/PortButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot>MyButton</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class PortButton extends Vue {}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <PortButton></PortButton>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import PortButton from "@/components/PortButton.vue";
 
 @Component({
   components: {
     HelloWorld,
-  },
+    PortButton
+  }
 })
 export default class Home extends Vue {}
 </script>
